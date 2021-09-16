cdfe1c754fc01fc1fc91f07ec624dd044a98c756
greg-Da <gregoiredalmasso@gmail.com>
2021-09-16 13:48:29 +0200

    [sidebar]: update design

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
-import { AiOutlineFileSearch } from 'react-icons/ai'
-import { IoArrowBackCircleOutline } from 'react-icons/io5'
+import { AiOutlineFileSearch, AiOutlineShoppingCart, AiOutlineGift, AiOutlineLineChart } from 'react-icons/ai'
+import { IoArrowBackCircleOutline, IoDocumentTextOutline } from 'react-icons/io5'
+import { RiDashboard3Line, RiSettings3Line, RiMailSendLine } from 'react-icons/ri'
+import { FaBoxes, FaRegAddressBook } from 'react-icons/fa'
 
 type Variant =
 | 'green'
@@ -31,20 +33,76 @@ export default function SideBar(props: Props) {
     <div style={{height: '100vh'}} className={`left-0 flex border-r border-gray-300 bg-white w-64 transform duration-500 ${open ? 'translate-x-0' : '-translate-x-52'}`}>
         <div className={`h-full z-50 w-1/4 transform duration-500 ${open ? 'translate-x-0' : 'translate-x-52'} ${variant === 'green' ? "bg-green-500" : "bg-gold-500"}`}>
             <ul>
+                <li className='flex h-16 border-b border-white'>
+                    <div className={`w-full w-1/4 flex justify-center items-center`}>
+                        <AiOutlineFileSearch className='text-white w-6 h-6'/>
+                    </div>
+                </li>
+
                 <li className='flex h-20 border-b border-white'>
-                    <div className={`w-full transform duration-500 w-1/4 flex justify-center items-center`}>
-                        <AiOutlineFileSearch className='text-white w-8 h-8'/>
+                    <div className={`${variant === 'green' ? "bg-green-500" : "bg-gold-500"} w-1/4 flex justify-center items-center`}></div>
+                </li>
+
+                <li className='flex h-14 border-b border-white'>
+                    <div className={`w-full w-1/4 flex justify-center items-center`}>
+                        <RiDashboard3Line className='text-white w-6 h-6'/>
                     </div>
                 </li>
 
                 <li className='flex h-14 border-b border-white'>
-                    <div className={`${variant === 'green' ? "bg-green-500" : "bg-gold-500"} transform duration-500 w-1/4 flex justify-center items-center`}></div>
+                    <div className={`w-full w-1/4 flex justify-center items-center`}>
+                        <AiOutlineShoppingCart className='text-white w-6 h-6'/>
+                    </div>
                 </li>
+
+                <li className='flex h-14 border-b border-white'>
+                    <div className={`w-full w-1/4 flex justify-center items-center`}>
+                        <FaBoxes className='text-white w-6 h-6'/>
+                    </div>
+                </li>
+
+                <li className='flex h-14 border-b border-white'>
+                    <div className={`w-full w-1/4 flex justify-center items-center`}>
+                        <AiOutlineGift className='text-white w-6 h-6'/>
+                    </div>
+                </li>
+
+                <li className='flex h-14 border-b border-white'>
+                    <div className={`w-full w-1/4 flex justify-center items-center`}>
+                        <FaRegAddressBook className='text-white w-6 h-6'/>
+                    </div>
+                </li>
+
+                <li className='flex h-14 border-b border-white'>
+                    <div className={`w-full w-1/4 flex justify-center items-center`}>
+                        <AiOutlineLineChart className='text-white w-6 h-6'/>
+                    </div>
+                </li>
+
+                <li className='flex h-14 border-b border-white'>
+                    <div className={`w-full w-1/4 flex justify-center items-center`}>
+                        <IoDocumentTextOutline className='text-white w-6 h-6'/>
+                    </div>
+                </li>
+
+                <li className='flex h-14 border-b border-white'>
+                    <div className={`w-full w-1/4 flex justify-center items-center`}>
+                        <RiSettings3Line className='text-white w-6 h-6'/>
+                    </div>
+                </li>
+
+                <li className='flex h-14 border-b border-white'>
+                    <div className={`w-full w-1/4 flex justify-center items-center`}>
+                        <RiMailSendLine className='text-white w-6 h-6'/>
+                    </div>
+                </li>
+
+
 
                 <div className='absolute bottom-0 w-full'>
                     <li onClick={() => setOpen(!open)} className='flex h-14 border-b border-white cursor-pointer'>
                         <div className={` w-full flex justify-center items-center`}>
-                            <IoArrowBackCircleOutline className={`text-white w-8 h-8 transform duration-500 ${open ? 'rotate-0' : 'rotate-180'}`} />
+                            <IoArrowBackCircleOutline className={`text-white w-6 h-6 transform duration-500 ${open ? 'rotate-0' : 'rotate-180'}`} />
                         </div>
                     </li>
 
@@ -60,93 +118,91 @@ export default function SideBar(props: Props) {
 
         <div className={`h-full w-full relative`}>
             <ul>
-                <li className='flex h-20 border-b border-white'>
+                <li className='flex h-16 border-b border-white'>
                     <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
                         {title}
                     </div>
                 </li>
 
+                <li className='flex h-20 border-b border-white'>
+                    <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
+                        <span>Bienvenue {name}</span>
+                    </div>
+                </li>
+
                 <li className='flex h-14 border-b border-white'>
-                <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
-                    <span>Bienvenue {name}</span>
-                </div>
-            </li>
+                    <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
+                        <a>Dashboard</a>
+                    </div>
+                </li>
+
+                <li className='flex h-14 border-b border-white'>
+                    <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
+                        <a>Mes commandes</a>
+                    </div>
+                </li>
+
+                <li className='flex h-14 border-b border-white'>
+                    <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
+                        <a>Gestion des stocks</a>
+                    </div>
+                </li>
+
+                <li className='flex h-14 border-b border-white'>
+                    <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
+                        <a>Gratuité & bons cadeaux</a>
+                    </div>
+                </li>
+
+                <li className='flex h-14 border-b border-white'>
+                    <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
+                        <a>Clients</a>
+                    </div>
+                </li>
+
+                <li className='flex h-14 border-b border-white'>
+                    <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
+                        <a>Rapport d'activité</a>
+                    </div>
+                </li>
+
+                <li className='flex h-14 border-b border-white'>
+                    <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
+                        <a>Géré ma facturation</a>
+                    </div>
+                </li>
+
+                <li className='flex h-14 border-b border-white'>
+                    <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
+                        <a>Paramétrage du compte</a>
+                    </div>
+                </li>
+
+                <li className='flex h-14 border-b border-white'>
+                    <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
+                        <a>Contacter Relax</a>
+                    </div>
+                </li>
             </ul>
 
             <div className='absolute bottom-0 w-full'>
-            <ul>
-            <li onClick={() => setOpen(!open)} className='flex h-14 border-b border-white cursor-pointer'>
-                <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
-                    <span>Réduire le menu</span>
-                </div>
-            </li>
-            <li className='flex h-12 border-b border-white'>
-                <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
-                    <a className='cursor-pointer text-sm'><span className='font-bold'>Mention légales</span> /CGU / CGV</a>
-                </div>
-            </li>
-            </ul>
+                <ul>
+                <li onClick={() => setOpen(!open)} className='flex h-14 border-b border-white cursor-pointer'>
+                    <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
+                        <span>Réduire le menu</span>
+                    </div>
+                </li>
+                <li className='flex h-12 border-b border-white'>
+                    <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
+                        <a className='cursor-pointer text-sm'><span className='font-bold'>Mention légales</span> /CGU / CGV</a>
+                    </div>
+                </li>
+                </ul>
+            </div>
+
         </div>
-
-        </div>
-        {/* <ul>
-            <li className='flex h-20 border-b border-white'>
-                <div className={`${variant === 'green' ? "bg-green-500" : "bg-gold-500"} transform duration-500 ${open ? 'translate-x-0' : 'translate-x-48'} w-1/4 flex justify-center items-center`}>
-                    <AiOutlineFileSearch className='text-white w-8 h-8'/>
-                </div>
-                <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
-                    {title}
-                </div>
-            </li>
-
-            <li className='flex h-14 border-b border-white'>
-                <div className={`${variant === 'green' ? "bg-green-500" : "bg-gold-500"} transform duration-500 ${open ? 'translate-x-0' : 'translate-x-48'} w-1/4 flex justify-center items-center`}>
-                </div>
-                <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
-                    <span>Bienvenue {name}</span>
-                </div>
-            </li>
-
-            <li className='h-full flex w-full'>
-            <div className={`${variant === 'green' ? "bg-green-500" : "bg-gold-500"} h-full transform duration-500 ${open ? 'translate-x-0' : 'translate-x-48'} w-1/4 flex justify-center items-center`}>
-                
-            </div>
-            <div className={`w-full h-full flex justify-center items-center border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
-            </div>
-        </li>
-        </ul>
-
-        <div className='h-full flex w-full'>
-            <div className={`${variant === 'green' ? "bg-green-500" : "bg-gold-500"} h-full transform duration-500 ${open ? 'translate-x-0' : 'translate-x-48'} w-1/4 flex justify-center items-center`}>
-                
-            </div>
-            <div className={`w-full h-full flex justify-center items-center border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
-            </div>
-        </div>
-
-
-        <div className='absolute bottom-0 w-full'>
-            <ul>
-            <li onClick={() => setOpen(!open)} className='flex h-14 border-b border-white cursor-pointer'>
-                <div className={`${variant === 'green' ? "bg-green-500" : "bg-gold-500"} transform duration-500 ${open ? 'translate-x-0' : 'translate-x-48'} w-1/4 flex justify-center items-center`}>
-                <IoArrowBackCircleOutline className={`text-white w-8 h-8 transform duration-500 ${open ? 'rotate-0' : 'rotate-180'}`} />
-                
-                </div>
-                <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
-                    <span>Réduire le menu</span>
-                </div>
-            </li>
-            <li className='flex h-12 border-b border-white'>
-                <div className={`${variant === 'green' ? "bg-green-500" : "bg-gold-500"} transform duration-500 ${open ? 'translate-x-0' : 'translate-x-48'} w-1/4 flex justify-center items-center`}>
-                </div>
-                <div className={`w-full flex justify-center items-center border-b border-gray-300 bg-white ${open ? '' : 'hidden'}`}>
-                    <a className='cursor-pointer text-sm'><span className='font-bold'>Mention légales</span> /CGU / CGV</a>
-                </div>
-            </li>
-            </ul>
-        </div> */}
     </div>
-  );
+);
 }
 
 export { SideBar };
